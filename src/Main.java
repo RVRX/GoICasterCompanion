3ebc6cf6400c15a4b39780e0be161ef0d25491e1
Cole Manning // RVRX <colemanning@yandex.com>
2021-06-16 15:56:09 -0400

    Add prototype for setTeam()

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to GoIStreamToolRedux CLI");
+
+
+    }
+
+    /**
+     * Sets the current team.
+     * E.g., used when setting "The Skyborne" as team A.
+     *
+     * Updates the output teamA/B image with the new team's image.
+     *  File is in the format `"Team" + teamIdentifier + ".png"`
+     * Updates the team & teamShort txt files to the new team's name.
+     *  File name formatted as `"Team" + teamIdentifier + ".txt"`,
+     *  and `"TeamShort" + teamIdentifier + ".txt"`
+     *
+     * @param newTeamName Name of the team
+     * @param teamIdentifier Either "A" or "B".
+     * @return success or fail
+     */
+    static boolean setTeam(String newTeamName, String teamIdentifier) {
+        return false;
+    }
+}
