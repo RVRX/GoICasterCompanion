91fa4246a3076f16ca3303ffb84b479f8741be8c
Cole Manning // RVRX <colemanning@yandex.com>
2021-06-16 16:27:36 -0400

    progress on setTeam()

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 public class Main {
+
+    //System independent path to output folder
+    public static String outputPath = System.getProperty("user.dir") + File.separator + "output" + File.separator;
+    //System independent path to input folder
+    public static String inputPath = System.getProperty("user.dir") + File.separator + "input" + File.separator;
+
     public static void main(String[] args) {
         System.out.println("Welcome to GoIStreamToolRedux CLI");
+
+        try {
+            setTeam("The Skyborne", "A");
+        } catch (IOException e) {
+            System.out.println("Failure in setTeam()!");
+            e.printStackTrace();
+        }
 
 
     }
@@ -18,8 +36,26 @@ public class Main {
      * @param newTeamName Name of the team
      * @param teamIdentifier Either "A" or "B".
      * @return success or fail
+     * @throws IOException
      */
-    static boolean setTeam(String newTeamName, String teamIdentifier) {
+    static boolean setTeam(String newTeamName, String teamIdentifier) throws IOException {
+
+        /*--- Update TXTs ---*/
+
+        //get file names
+        String teamTXT = "Team" + teamIdentifier + ".txt";
+        String teamShortTXT = "TeamShort" + teamIdentifier + ".txt";
+
+        //update output file "TeamX.txt"
+        Writer fileWriter = new FileWriter(outputPath + teamTXT); //TeamX.txt
+        fileWriter.write(newTeamName);
+        fileWriter.close();
+
+        //update output file "TeamShortX.txt"
+        Writer fileWriter2 = new FileWriter(outputPath + teamShortTXT); //TeamShortX.txt
+        fileWriter2.write("TODO SHORTNAME GOES HERE"); //todo get shortname from teams.txt
+        fileWriter2.close();
+
         return false;
     }
 }
