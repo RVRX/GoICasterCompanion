86a0bca4c6b8f13e9794bbd89de5242fa14e2643
Cole Manning // RVRX <colemanning@yandex.com>
2021-06-20 14:47:34 -0400

    Create Default page and controller

--- src/main/java/goistreamtoolredux/controller/Default.java
+++ src/main/java/goistreamtoolredux/controller/Default.java
@@ -1,4 +1,11 @@
 package goistreamtoolredux.controller;
 
+import javafx.event.ActionEvent;
+
 public class Default {
+    public void selectTeamA(ActionEvent actionEvent) {
+    }
+
+    public void selectTeamB(ActionEvent actionEvent) {
+    }
 }
