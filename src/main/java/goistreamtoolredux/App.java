9bb30f59f48429c93714d627e2ba4bf83bb6bcff
Cole Manning // RVRX <colemanning@yandex.com>
2021-06-22 18:29:09 -0400

    add verification on start-up

--- src/main/java/goistreamtoolredux/App.java
+++ src/main/java/goistreamtoolredux/App.java
@@ -1,7 +1,9 @@
 package goistreamtoolredux;
 
 import goistreamtoolredux.algorithm.CustomTimer;
+import goistreamtoolredux.algorithm.FileManager;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,6 +16,12 @@ public class App extends Application {
     @Override
     public void init() {
         System.out.println("Starting Up");
+        try {
+            FileManager.verifyContent();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            Platform.exit();
+        }
     }
 
     @Override
