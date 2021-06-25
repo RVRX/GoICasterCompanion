a46fa607baae8996e20f05aead46172c5e7acf54
Cole Manning // RVRX <colemanning@yandex.com>
2021-06-25 15:38:17 -0400

    messy setup to lobby timer
    Live timer counter does not update

--- src/main/java/goistreamtoolredux/controller/TimerPane.java
+++ src/main/java/goistreamtoolredux/controller/TimerPane.java
@@ -1,10 +1,16 @@
 package goistreamtoolredux.controller;
 
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+import goistreamtoolredux.algorithm.CustomTimer;
+import goistreamtoolredux.algorithm.FileManager;
+import goistreamtoolredux.algorithm.InvalidDataException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -48,12 +54,12 @@ public class TimerPane {
 
     @FXML
     void customPauseClicked(MouseEvent event) {
-
+        System.out.println("customPauseClicked");
     }
 
     @FXML
     void customPlayClicked(MouseEvent event) {
-
+        System.out.println("customPlayClicked");
     }
 
     @FXML
@@ -68,22 +74,44 @@ public class TimerPane {
 
     @FXML
     void lobbyPauseClicked(MouseEvent event) {
-
+        CustomTimer.getInstance().pause();
     }
 
     @FXML
     void lobbyPlayClicked(MouseEvent event) {
-
+        try {
+            CustomTimer.getInstance().start();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            //todo handle
+        } catch (InvalidDataException e) {
+            e.printStackTrace();
+            //todo handle
+        }
     }
 
     @FXML
     void lobbyRestartClicked(MouseEvent event) {
+        try {
+            CustomTimer.getInstance().restart();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            //todo handle
+        } catch (InvalidDataException e) {
+            e.printStackTrace();
+            //todo handle
+        }
 
     }
 
     @FXML
     void lobbyStopClicked(MouseEvent event) {
-
+        try {
+            CustomTimer.getInstance().stop();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            //todo handle
+        }
     }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -98,6 +126,29 @@ public class TimerPane {
         assert customStop != null : "fx:id=\"customStop\" was not injected: check your FXML file 'TimerPane.fxml'.";
         assert customRestart != null : "fx:id=\"customRestart\" was not injected: check your FXML file 'TimerPane.fxml'.";
         assert customTimerText1 != null : "fx:id=\"customTimerText1\" was not injected: check your FXML file 'TimerPane.fxml'.";
+
+        //get initial lobby timer length
+        try {
+            lobbyTimerText.setText(String.valueOf(CustomTimer.getInstance().get()));
+        } catch (FileNotFoundException exception) {
+            //if file is not found - then there is no current timer, so the default '00:00' is what we want
+            // so no changes need to be made
+            System.out.println("Timer File not created, 00:00 will be used");
+            //BUT, without timer file, timer commands will error out so it must be created
+            File timerOut = new File(FileManager.outputPath + "Timer.txt");
+            try {
+                timerOut.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                //todo, handle failed file creation
+            }
+        } catch (InvalidDataException invalidDataException) {
+            invalidDataException.printStackTrace();
+            //todo handle
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            //todo handle
+        }
 
     }
 }
