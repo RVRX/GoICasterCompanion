870c0ef47a851d4e9322d2c711f30711b10eb590
Cole Manning // RVRX <colemanning@yandex.com>
2021-07-07 22:28:19 -0400

    fix lobby timer not starting bug

--- src/main/java/goistreamtoolredux/App.java
+++ src/main/java/goistreamtoolredux/App.java
@@ -12,10 +12,17 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class App extends Application {
 
@@ -95,6 +102,41 @@ public class App extends Application {
         if (LobbyTimer.getInstance().getCurrentTimer() != null) {
             LobbyTimer.getInstance().getCurrentTimer().cancel();
         }
+    }
+
+    public static void showExceptionDialog(Exception exception, String headerText, String contentText) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Exception Dialog");
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+
+        // Create expandable Exception.
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        exception.printStackTrace(pw);
+        String exceptionText = sw.toString();
+
+        Label label = new Label("The exception stacktrace was:");
+
+        TextArea textArea = new TextArea(exceptionText);
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(label, 0, 0);
+        expContent.add(textArea, 0, 1);
+
+// Set expandable Exception into the dialog pane.
+        alert.getDialogPane().setExpandableContent(expContent);
+
+        System.out.println("posting");
+        alert.showAndWait();
     }
 }
 
