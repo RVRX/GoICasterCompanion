3d7cd1635f5699e1d921430470e6857f3f4ab593
Cole Manning // RVRX <colemanning@yandex.com>
2021-07-16 17:51:50 -0400

    remove defunct tests

--- src/test/java/goistreamtoolredux/AlgorithmTest.java
+++ src/test/java/goistreamtoolredux/AlgorithmTest.java
@@ -1,7 +1,6 @@
 package goistreamtoolredux;
 
 import goistreamtoolredux.algorithm.FileManager;
-import goistreamtoolredux.algorithm.InvalidDataException;
 import goistreamtoolredux.algorithm.LobbyTimer;
 import goistreamtoolredux.algorithm.Team;
 import javafx.collections.FXCollections;
@@ -15,48 +14,12 @@ import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 /**
  * Tests for Algorithm (Non-UI tests)
  */
 public class AlgorithmTest {
-
-    @Test
-    public void settingAndGettingInitialTimerTest() throws IOException, InvalidDataException {
-        LobbyTimer lobbyTimer = LobbyTimer.getInstance();
-
-        //10
-        lobbyTimer.setInitialTimerLength(10);
-        assertEquals(10, lobbyTimer.getInitialTimerLength());
-
-
-        //120
-        lobbyTimer.setInitialTimerLength(120);
-        assertEquals(120, lobbyTimer.getInitialTimerLength());
-
-        Exception exception = assertThrows(NumberFormatException.class, () -> {
-            Integer.parseInt("1a");
-        });
-    }
-
-    /**
-     * setting initial value to 0 is not allowed. Would cause a loop of
-     * <pre>start() -> restart() -> start()</pre>
-     * due to start calling restart if timer is 0, and restart starting
-     * from the initial value (which would be 0), then calling start.
-     */
-    @Test
-    public void exceptionSetInitialTimerInvalidTest() {
-        //setting initial value to 0 is not allowed. Would cause a "start() -> restart() -> start()" loop
-        // due to start calling restart if timer is 0, and restart starting from the initial value (which would be 0)
-        // then calling start
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-           LobbyTimer.getInstance().setInitialTimerLength(0);
-        });
-    }
-
 
     @Test
     public void readTeamsFromDiskTest() throws IOException {
